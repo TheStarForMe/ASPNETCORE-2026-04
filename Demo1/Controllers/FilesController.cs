91b176469d6e08896cf4b0de9a851b458c6d1581
Simon Stirling <simonsbs@gmail.com>
2026-04-23 12:21:36 +0300

    .

---
+++ Demo1/Controllers/FilesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo1.Controllers {
+    [ApiController]
+    [Route("api/files")]
+    public class FilesController : ControllerBase {
+
+        [HttpGet("{name}")]
+        public ActionResult GetFile(string name) {
+            var path = Path.Combine("Files", name);
+
+            if (!System.IO.File.Exists(path)) {
+                return NotFound();
+            }
+
+            var data = System.IO.File.ReadAllBytes(path);
+
+            return File(data, "text/plain", name);
+        }
+    }
+}
