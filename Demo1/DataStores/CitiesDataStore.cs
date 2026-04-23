b3a202dd48e74d780d0a08399e846a0f810037e8
Simon Stirling <simonsbs@gmail.com>
2026-04-23 09:39:23 +0300

    .

--- Demo1/DataStores/CitiesDataStore.cs
+++ Demo1/DataStores/CitiesDataStore.cs
@@ -6,8 +6,37 @@ namespace Demo1.DataStores {
 
         static CitiesDataStore() {
             Current = new List<CityDTO>() {
-                new CityDTO{ID = 1, Name = "Tel-Aviv"},
-                new CityDTO{ID = 2, Name = "Jerusalem"},
+                new CityDTO{
+                    ID = 1,
+                    Name = "Tel-Aviv",
+                    LandMarks = new List<LandMarkDTO>{
+                        new LandMarkDTO {
+                            ID = 1,
+                            Name = "Azrieli Towers",
+                            Description = "A complex of three skyscrapers"
+                        },
+                        new LandMarkDTO {
+                            ID = 2,
+                            Name = "Dizengoff Square",
+                            Description = "A public square in the center of Tel-Aviv"
+                        }
+                    }},
+                new CityDTO{
+                    ID = 2,
+                    Name = "Jerusalem",
+                    LandMarks = new List<LandMarkDTO>{
+                        new LandMarkDTO {
+                            ID = 3,
+                            Name = "Western Wall",
+                            Description = "A wall in the Old City of Jerusalem"
+                        },
+                        new LandMarkDTO {
+                            ID = 4,
+                            Name = "Church of the Holy Sepulchre",
+                            Description = "A church in the Old City of Jerusalem"
+                        }
+                    }
+                },
             };
         }
     }
