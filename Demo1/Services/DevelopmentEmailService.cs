6b62dafb9e55a7c13b1b151c400ae970f591e4fc
Simon Stirling <simonsbs@gmail.com>
2026-04-30 15:59:48 +0300

    Inject IConfiguration into DevelopmentEmailService
    Refactored DevelopmentEmailService to use IConfiguration for loading email addresses from configuration files. Updated appsettings.json, appsettings.Development.json, and appsettings.Production.json to include appropriate "mail:from" and "mail:to" settings for each environment.

--- Demo1/Services/DevelopmentEmailService.cs
+++ Demo1/Services/DevelopmentEmailService.cs
@@ -1,13 +1,16 @@
 ﻿namespace Demo1.Services {
     public class DevelopmentEmailService : IEmailService {
+        private IConfiguration _config;
         private string _to;
         private string _from;
 
         private Guid _id;
 
-        public DevelopmentEmailService() {
-            _to = "simon@email.com";
-            _from = "api@email.com";
+        public DevelopmentEmailService(IConfiguration config) {
+            _config = config ?? throw new ArgumentNullException(nameof(config));
+
+            _to = _config["mail:to"] ?? throw new ArgumentNullException("mail:to");
+            _from = _config["mail:from"] ?? throw new ArgumentNullException("mail:from");
 
             _id = Guid.NewGuid();
         }
