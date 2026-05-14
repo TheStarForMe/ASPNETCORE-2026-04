08ca2650ddf34e452a08f12a76ef5c0359ceea00
Simon Stirling <simonsbs@gmail.com>
2026-05-14 14:41:16 +0300

    Add custom Feature to FeatureDto mapping and FeatureMapper
    Added MapFeatureText for custom FeatureDto text formatting.
    Introduced FeatureMapper with Mapperly and manual mapping methods.
    Removed old commented FeatureMapper code.
    Updated ProductMapper to use new FeatureDto mapping logic.

--- MapDemo/MapperlyDemo/Mappings/ProductMapper.cs
+++ MapDemo/MapperlyDemo/Mappings/ProductMapper.cs
@@ -5,16 +5,36 @@ using Riok.Mapperly.Abstractions;
 namespace MapDemo.MapperlyDemo.Mappings {
     [Mapper]
     public partial class ProductMapper {
-        [MapperIgnoreSource(nameof(Product.Features))]
         [MapperIgnoreSource(nameof(Product.Notes))]
         public partial ProductDto ToDto(Product product);
 
 
         public partial List<ProductDto> ToDtos(List<Product> products);
 
+
+        [MapPropertyFromSource(nameof(FeatureDto.Text), Use = nameof(MapFeatureText))]
+        public partial FeatureDto ToDto(Feature feature);
+
+        private static string MapFeatureText(Feature feature) {
+            return $"** {feature.Key}: {feature.Value} --";
+        }
     }
 
-    //public partial class FeatureMapper {
-    //    public partial FeatureDto ToDto(Feature feature);
-    //}
+
+    [Mapper]
+    public partial class FeatureMapper {
+        [MapProperty(nameof(Feature.Key), nameof(FeatureDto.Text), Use = nameof(MapFeatureText))]
+        public partial FeatureDto ToDto(Feature feature);
+
+        // kind of silly, we dont use mapperly for this, but it shows that we can use the same mapping method in multiple mappers
+        public FeatureDto ToDto_v2(Feature feature) {
+            return new FeatureDto {
+                Text = MapFeatureText(feature)
+            };
+        }
+
+        public static string MapFeatureText(Feature feature) {
+            return $"** {feature.Key}: {feature.Value} --";
+        }
+    }
 }
